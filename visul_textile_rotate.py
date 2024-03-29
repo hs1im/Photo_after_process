6d03033f17d8c3f203e5c4699afe6cbf1dadd629
Ast@tin <4922865@gmail.com>
2024-03-29 20:32:32 +0900

    Program of loop all file

---
+++ visul_textile_rotate.py
@@ -0,0 +1,9 @@
+# This program is used to loop all the images in the folder
+
+import image_crop
+sourceDest='4차_데이터'
+targetDest='4th_data_frames/average'
+numbers=['1','0','2','3','4','5','6','7','8','9']
+lux=['1000LUX','2000LUX','3000LUX','4000LUX','5000LUX','6000LUX','7000LUX','7500LUX']
+distance=['10cm','15cm','20cm','25cm','30cm','35cm','40cm']
+degree=['+45deg','+30deg','+15deg','0deg','-15deg','-30deg','-45deg']
