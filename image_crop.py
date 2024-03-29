efd160a8900d287b5b6baf59d025dd4a602436f9
Ast@tin <4922865@gmail.com>
2024-03-29 16:43:19 +0900

    process of splice to rectangel

--- image_crop.py
+++ image_crop.py
@@ -1,65 +1,63 @@
 import cv2
+import numpy as np
 
-def display_image(image_path):
-    image = cv2.imread(image_path)
-    cv2.imshow('Image', image)
-    cv2.waitKey(0)
-    cv2.destroyAllWindows()
+# Load the image
+img = cv2.imread('Data/Original/0/4000LUX_10cm_+15deg.png')
 
-# replace 'your_image_path.jpg' with the path to the image you want to display
+# Create a clone of the image to draw on
+img_clone = img.copy()
 
+cv2.namedWindow('image')
 
+# Initial point
+ix, iy = -1, -1
 
-rectangles = []
-current_rectangle = []
-cropping = False
+# Image cutting  function
+def slice_image(img, start, end):
+    # Check x and y to not twist it's coordinate
+    for i in range(2):
+        if(start[i]>end[i]):
+            ms=start[i]
+            start[i]=end[i]
+            end[i]=ms
 
-def click_and_crop(event, x, y, flags, param):
-    # grab references to the global variables
-    global rectangles, cropping, current_rectangle
+    # Return sliced image
+    return img[start[1]:end[1], start[0]:end[0]]
 
-    # if the left mouse button was clicked, record the starting
-    # (x, y) coordinates and indicate that cropping is being
-    # performed
+# Mouse callback function
+def draw_rectangle(event, x, y, flags, param):
+    global ix, iy, img, img_clone
     if event == cv2.EVENT_LBUTTONDOWN:
-        current_rectangle = [(x, y)]
-        cropping = True
+        if param[0] : 
+            print(f'({ix},{iy}),({x},{y}),width={abs(ix-x)},height={abs(iy-y)}')
+            cv2.imshow('aaa',slice_image(img_clone,[ix,iy],[x,y]))
+            param[1]=True
+            param[0]=False
+        else :
+            if(param[1]):
+                cv2.destroyWindow('aaa')
+                param[1]=False
+            ix, iy = x, y
+            param[0]=True
+    elif event == cv2.EVENT_MOUSEMOVE:
+        if ix != -1 and iy != -1:
+            img_clone = img.copy()
+            cv2.rectangle(img_clone, (ix, iy), (x, y), (255,255,0), 1)
+            
+            
+        
+# Set parameter to use [sw,windowOpened]
+    # sw is switch of checking 'is it clicked?'
+    # windowOpened is switch of checking 'is window opened?'
+param=[False,False]
 
-    # check to see if the left mouse button was released
-    elif event == cv2.EVENT_LBUTTONUP:
-        # record the ending (x, y) coordinates
-        current_rectangle.append((x, y))
-        cropping = False # cropping is finished
+# Bind the function to window
 
-        cv2.rectangle(image, current_rectangle[0], current_rectangle[1], (0, 255, 0), 2)
-        cv2.imshow("image", image)
+cv2.setMouseCallback('image', draw_rectangle,param)
 
-# load the image, clone it, and setup the mouse callback function
-image = cv2.imread('your_image_path.jpg')
-clone = image.copy()
-cv2.namedWindow("image")
-cv2.setMouseCallback("image", click_and_crop)
-
-# keep looping until the 'q' key is pressed
-while True:
-    # display the image and wait for a keypress
-    cv2.imshow("image", image)
-    key = cv2.waitKey(1) & 0xFF
-
-    # if the 'r' key is pressed, reset the cropping region
-    if key == ord("r"):
-        image = clone.copy()
-
-    # if the 'c' key is pressed, break from the loop
-    elif key == ord("c"):
+while(1):
+    cv2.imshow('image', img_clone)
+    if cv2.waitKey(20) & 0xFF == 27:  # press ESC to exit
         break
 
-# if there are two points, then crop the region of interest
-# from the image and display it
-if len(current_rectangle) == 2:
-    roi = clone[current_rectangle[0][1]:current_rectangle[1][1], current_rectangle[0][0]:current_rectangle[1][0]]
-    cv2.imshow("ROI", roi)
-    cv2.waitKey(0)
-
-# close all open windows
 cv2.destroyAllWindows()
