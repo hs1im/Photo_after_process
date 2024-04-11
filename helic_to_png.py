32892aca7468f8a9d08df7c075e0a412e9e584b1
Ast@tin <4922865@gmail.com>
2024-04-11 21:15:21 +0900

    pyheif is not available to windows.. so, I give up to make convert file..

--- helic_to_png.py
+++ helic_to_png.py
@@ -1,15 +1,25 @@
 import os
+import pyheif
 from PIL import Image
 
 def convert_heic_to_png(heic_file, png_file):
-    img = Image.open(heic_file)
+    heif_file = pyheif.read(heic_file)
+    img = Image.frombytes(
+        heif_file.mode, 
+        heif_file.size, 
+        heif_file.data,
+        "raw",
+        heif_file.mode,
+        heif_file.stride,
+    )
     img.save(png_file, 'PNG')
 
-source=""
-dest=""
+source="./Data/second_origin"
+dest="./Data/second_png"
 
-for filename in os.listdir(path):
+for filename in os.listdir(source):
     if filename.endswith('.heic') or filename.endswith('.HEIC'):
-        heic_file = os.path.join(path, filename)
-        png_file = os.path.join(path, os.path.splitext(filename)[0] + '.png')
+        heic_file = f"{source}/{filename}"
+        png_file = f"{dest}/{os.path.splitext(filename)[0]}.png"
+        #png_file = os.path.join(dest, os.path.splitext(filename)[0] + '.png')
         convert_heic_to_png(heic_file, png_file)
